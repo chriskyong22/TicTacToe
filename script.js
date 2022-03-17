9fc2b77f5279f5504e4969a2659c056d9c0401fd
chriskyong22 <55605854+chriskyong22@users.noreply.github.com>
2022-03-17 15:14:01 -0400

    Made the arguments be type number instead of type string

--- script.js
+++ script.js
@@ -9,24 +9,53 @@
     resetBoard
 */
 
-console.log("Test");
-console.log(gameBoard.getBoard());
-
 var currentTurnInput = document.querySelector('.current-turn-text');
+var resetBtn = document.querySelector('.reset-game-btn');
+resetBtn.addEventListener('click', resetBoard);
 
 function updateCurrentTurn() {
     currentTurnInput.textContent = gameBoard.getCurrentTurn();
 }
 
+function setBoard() {
+    let board = gameBoard.getBoard();
+    let cells = getBoardCells();
+    for (let cell of cells) {
+        let [row, col] = cell.getAttribute('data-row-column').split(',');
+        cell.textContent = board[row][col];
+    }
+}
+
 function initBoard() {
+    let cells = getBoardCells();
+    for (let cell of cells) {
+        let [row, col] = cell.getAttribute('data-row-column').split(',');
+        cell.addEventListener('click', setCell);
+    }
+    setBoard();
+}
+
+function setCell(event) {
+    let [row, col] = event.currentTarget.getAttribute('data-row-column').split(',');
+    console.log(gameBoard.move(parseInt(row), parseInt(col)));
+    event.currentTarget.textContent = gameBoard.getBoard()[row][col];
+    updateCurrentTurn();
+}
+
+function* getBoardCells() {
     let board = gameBoard.getBoard();
     for (let row = 0; row < board.length; row++) {
         for (let col = 0; col < board[row].length; col++) {
             let dataAttribute = `${row},${col}`;
-            let cell = document.querySelector(`[data-row-column="${dataAttribute}"]`);
-            cell.textContent = board[row][col];
+            yield document.querySelector(`[data-row-column="${dataAttribute}"]`);
         }
     }
+}
+
+function resetBoard() {
+    gameBoard.resetBoard();
+    setBoard();
+    updateCurrentTurn();
 }
 
 initBoard();
